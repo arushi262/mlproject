6fcc7fbf778534ee95e528985526944d456641db
Arushi Thakur <arushithakur@Arushis-MacBook-Air.local>
2023-06-15 22:37:14 -0400

    jupyter nbs

--- src/exception.py
+++ src/exception.py
@@ -1,23 +1,37 @@
 import sys
+from src.logger import logging
+from exception import CustomException
 
-def error_message_detail(error, error_detail:sys) :
-    '''
-        Will give information about the error -> file, line number location
-    '''
-    _,_,exc_tb = error_detail.exc_info()
+
+def error_message_detail(error, error_detail: sys):
+    """
+    Will give information about the error -> file, line number location
+    """
+    _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error ocurred in python script : [{0}] at line : [{1}] error : [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
+    error_message = (
+        "Error ocurred in python script : [{0}] at line : [{1}] error : [{2}]".format(
+            file_name, exc_tb.tb_lineno, str(error)
+        )
     )
-    
+
     return error_message
 
+
 class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail
+        )
 
-    def __init__(self, error_message, error_detail:sys):
-       super().__init__(error_message) 
-       self.error_message = error_message_detail(error_message,error_detail=error_detail)
-
-    
-    def __str__(self) :
+    def __str__(self):
         return self.error_message
+
+    if __name__ == "__main__":
+        try:
+            a = 1 / 0
+
+        except Exception as e:
+            logging.info("Divide by zero")
+            raise CustomException(e, sys)
--- src/logger.py
+++ src/logger.py
@@ -8,24 +8,16 @@ LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 # logs will be stored in a folder named 'log' with the above file name convetion
 
-logs_path = os.path.join(os.getcwd(),"log",LOG_FILE)
-os.makedirs(logs_path,exist_ok = True)
+logs_path = os.path.join(os.getcwd(), "log", LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
-    filename = LOG_FILE_PATH,
-    format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level = logging.INFO
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
 
 if __name__ == "__main__":
-
-    try:
-        a = 1/0
-    
-    except Exception as e:
-        logging.info('Divide by zero')
-        raise exception.CustomException(e,sys)
-
     logging.info("Logging begins here")
